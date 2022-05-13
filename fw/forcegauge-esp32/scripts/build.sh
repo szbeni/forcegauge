6f7045e9376f88b2b7f3315b288d455e642a8f29
szbeni <szbeni@gmail.com>
2022-05-13 19:12:19 +0100

    CI fix

--- fw/forcegauge-esp32/scripts/build.sh
+++ fw/forcegauge-esp32/scripts/build.sh
@@ -6,13 +6,12 @@ cd "$(dirname "$0")/.."
 DATE=$(date --iso-8601=seconds | tr : - | tr T _ | grep -oP '^.*\d\+' | grep -oP '^.*\d')
 mkdir -p ./build/$DATE
 
-export ARDUINO_DIRECTORIES_USER=$(pwd)
-export ARDUINO_OUTPUT_DIR=./build/$DATE
-export ARDUINO_ADDITIONAL_URLS=https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
-export ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
+set ARDUINO_OUTPUT_DIR=./build/$DATE
+set ARDUINO_ADDITIONAL_URLS=https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+set ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
 
 arduino-cli core update-index
-arduino-cli core install esp32:esp32
+arduino-cli core install esp32:esp32 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 
 #Adafruit_BusIO - https://github.com/adafruit/Adafruit_BusIO
 #Adafruit GFX Library - https://github.com/adafruit/Adafruit-GFX-Library
