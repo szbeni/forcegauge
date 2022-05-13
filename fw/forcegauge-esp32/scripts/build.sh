68a2f24d2c5d25dea5b592e8569c03b07903d511
szbeni <szbeni@gmail.com>
2022-05-13 19:16:49 +0100

    CI

--- fw/forcegauge-esp32/scripts/build.sh
+++ fw/forcegauge-esp32/scripts/build.sh
@@ -7,11 +7,14 @@ DATE=$(date --iso-8601=seconds | tr : - | tr T _ | grep -oP '^.*\d\+' | grep -oP
 mkdir -p ./build/$DATE
 
 set ARDUINO_OUTPUT_DIR=./build/$DATE
-set ARDUINO_ADDITIONAL_URLS=https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 set ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
 
-arduino-cli core update-index
-arduino-cli core install esp32:esp32 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+ESP32_URL=https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+GLOBAL_FLAGS= --additional-urls $ESP32_URL
+
+
+arduino-cli core update-index $GLOBAL_FLAGS
+arduino-cli core install esp32:esp32 --additional-urls $GLOBAL_FLAGS
 
 #Adafruit_BusIO - https://github.com/adafruit/Adafruit_BusIO
 #Adafruit GFX Library - https://github.com/adafruit/Adafruit-GFX-Library
