a43214bab939de651ad9e9eb77a031405aaca0e3
szbeni <szbeni@gmail.com>
2022-05-13 19:33:40 +0100

    CI

--- fw/forcegauge-esp32/scripts/build.sh
+++ fw/forcegauge-esp32/scripts/build.sh
@@ -6,9 +6,8 @@ cd "$(dirname "$0")/.."
 DATE=$(date --iso-8601=seconds | tr : - | tr T _ | grep -oP '^.*\d\+' | grep -oP '^.*\d')
 mkdir -p ./build/$DATE
 
-set ARDUINO_OUTPUT_DIR=./build/$DATE
-set ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
-
+export ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
+ARDUINO_OUTPUT_DIR=./build/$DATE
 ESP32_URL="https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
 GLOBAL_FLAGS="--additional-urls $ESP32_URL"
 
