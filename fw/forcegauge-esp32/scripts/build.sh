29dad30d3db1322e3a283e318cd2d9266ef68db8
szbeni <szbeni@gmail.com>
2022-05-13 20:34:12 +0100

    CI

--- fw/forcegauge-esp32/scripts/build.sh
+++ fw/forcegauge-esp32/scripts/build.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-set -e
-cd "$(dirname "$0")/.."
 
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=`dirname $SCRIPT`
+
+cd "$SCRIPT_DIR/.."
 
 DATE=$(date --iso-8601=seconds | tr : - | tr T _ | grep -oP '^.*\d\+' | grep -oP '^.*\d')
 mkdir -p ./build/$DATE
 
-export ARDUINO_LIBRARY_ENABLE_UNSAFE_INSTALL=true
 ARDUINO_OUTPUT_DIR=./build/$DATE
-ESP32_URL="https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
-GLOBAL_FLAGS="--additional-urls $ESP32_URL"
+CONFIG_FILE="--config-file $SCRIPT_DIR/arduino-cli.yaml"
 
+# Update boards
+arduino-cli core update-index $CONFIG_FILE
+arduino-cli core install esp32:esp32 $CONFIG_FILE 
 
-arduino-cli core update-index $GLOBAL_FLAGS
-arduino-cli core install esp32:esp32 $GLOBAL_FLAGS
-
+# Install libraries
 #Adafruit_BusIO - https://github.com/adafruit/Adafruit_BusIO
 #Adafruit GFX Library - https://github.com/adafruit/Adafruit-GFX-Library
 #Adafruit SSD1306 - https://github.com/adafruit/Adafruit_SSD1306
@@ -22,7 +23,8 @@ arduino-cli core install esp32:esp32 $GLOBAL_FLAGS
 #RingBuffer - https://github.com/Locoduino/RingBuffer
 #arduinoWebSockets - https://github.com/Links2004/arduinoWebSockets
 
-arduino-cli lib install "Adafruit GFX Library" "Adafruit SSD1306" "ArduinoJson" "RingBuffer"
-arduino-cli lib install --git-url https://github.com/Links2004/arduinoWebSockets
+arduino-cli lib install "Adafruit GFX Library" "Adafruit SSD1306" "ArduinoJson" "RingBuffer" $CONFIG_FILE
+arduino-cli lib install --git-url https://github.com/Links2004/arduinoWebSockets $CONFIG_FILE
 
-exec arduino-cli compile -v -b esp32:esp32:esp32c3 --output-dir=$ARDUINO_OUTPUT_DIR
+# Compile
+arduino-cli compile -v -b esp32:esp32:esp32c3 --output-dir=$ARDUINO_OUTPUT_DIR $CONFIG_FILE
