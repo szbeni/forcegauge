dd8d0904136ee5cc40fb5506d1d257cc18776b8a
szbeni <szbeni@gmail.com>
2024-06-09 00:30:12 +0100

    updated build with nimble

--- fw/forcegauge-esp32/scripts/build.sh
+++ fw/forcegauge-esp32/scripts/build.sh
@@ -24,29 +24,33 @@ arduino-cli core install esp32:esp32 $CONFIG_FILE
 #RingBuffer - https://github.com/Locoduino/RingBuffer
 #arduinoWebSockets - https://github.com/Links2004/arduinoWebSockets
 
-# Used library         Version Path                                                                                
-# WiFi                 2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/WiFi            
-# WebServer            2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/WebServer       
-# ESPmDNS              2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/ESPmDNS         
-# arduinoWebSockets    2.3.7   /home/beni/Arduino/libraries/arduinoWebSockets                                      
-# WiFiClientSecure     2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/WiFiClientSecure
-# Update               2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/Update          
-# DNSServer            2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/DNSServer       
-# RingBuffer           1.0.3   /home/beni/Arduino/libraries/RingBuffer                                             
-# SPIFFS               2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/SPIFFS          
-# FS                   2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/FS              
-# ArduinoJson          6.19.4  /home/beni/Arduino/libraries/ArduinoJson                                            
-# Wire                 2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/Wire            
-# Adafruit_GFX_Library 1.11.3  /home/beni/Arduino/libraries/Adafruit_GFX_Library                                   
-# Adafruit_BusIO       1.13.2  /home/beni/Arduino/libraries/Adafruit_BusIO                                         
-# SPI                  2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/SPI             
-# Adafruit_SSD1306     2.5.7   /home/beni/Arduino/libraries/Adafruit_SSD1306                                       
-# HTTPClient           2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4/libraries/HTTPClient      
-#Used platform Version Path                                                     
-#esp32:esp32   2.0.4   /home/beni/.arduino15/packages/esp32/hardware/esp32/2.0.4
+# Used library         Version Path
+# WiFi                 2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/WiFi
+# Networking           1.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/Network
+# WebServer            2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/WebServer
+# FS                   2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/FS
+# ESPmDNS              2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/ESPmDNS
+# WebSockets           2.4.1   /home/beni/Arduino/libraries/WebSockets
+# NetworkClientSecure  2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/NetworkClientSecure
+# Update               2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/Update
+# DNSServer            2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/DNSServer
+# ESP32 Async UDP      2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/AsyncUDP
+# RingBuffer           1.0.5   /home/beni/Arduino/libraries/RingBuffer
+# SPIFFS               2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/SPIFFS
+# ArduinoJson          7.0.4   /home/beni/Arduino/libraries/ArduinoJson
+# NimBLE-Arduino       1.4.1   /home/beni/Arduino/libraries/NimBLE-Arduino
+# Wire                 2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/Wire
+# Adafruit GFX Library 1.11.9  /home/beni/Arduino/libraries/Adafruit_GFX_Library
+# Adafruit BusIO       1.16.1  /home/beni/Arduino/libraries/Adafruit_BusIO
+# SPI                  2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/SPI
+# Adafruit SSD1306     2.5.10  /home/beni/Arduino/libraries/Adafruit_SSD1306
+# HTTPClient           2.0.0   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1/libraries/HTTPClient
+
+# Used platform Version Path
+# esp32:esp32   3.0.1   /home/beni/.arduino15/packages/esp32/hardware/esp32/3.0.1
 
 
-arduino-cli lib install "Adafruit GFX Library" "Adafruit SSD1306" "ArduinoJson" "RingBuffer" $CONFIG_FILE
+arduino-cli lib install "Adafruit GFX Library" "Adafruit SSD1306" "ArduinoJson" "RingBuffer" "NimBLE-Arduino" $CONFIG_FILE
 arduino-cli lib install --git-url https://github.com/Links2004/arduinoWebSockets $CONFIG_FILE
 
 # Compile
